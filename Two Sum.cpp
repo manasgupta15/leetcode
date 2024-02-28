6531099e8a62af24df6ef5dad20cac3f26582f02
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-02-28 09:08:19 +0530

    Create Two Sum.cpp

---
+++ Two Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        map < int, int > mpp;
+        for(int i = 0; i<n ;i++) {
+            int num = nums[i];
+            int more = target - num;
+            if(mpp.find(more) != mpp.end()) {
+                return {mpp[more],i};
+            }
+            mpp[num] = i;
+        }
+        return {-1,-1};
+    }
+};
