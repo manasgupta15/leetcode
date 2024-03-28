564f3dfc33761c9f8bc2a76a94aa52521fdce3a3
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-03-28 08:07:30 +0530

    Create Concatenation of Array.cpp

---
+++ Concatenation of Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int size = nums.size();
+        nums.resize(2 * size);
+
+        for(int i = 0;i<size;i++) {
+            nums[i+size] = nums[i];
+        }
+
+        return nums;
+    }
+};
