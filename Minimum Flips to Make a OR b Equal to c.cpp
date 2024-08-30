e78baa20702c1278cef1e27330b9426fd2cd0bbe
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-08-30 07:33:02 +0530

    Create Minimum Flips to Make a OR b Equal to c.cpp

---
+++ Minimum Flips to Make a OR b Equal to c.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+
+        // approach 1
+        
+        int flips = 0;
+
+        while (a != 0 || b != 0 || c != 0) {
+            // right most bit(MSB) == 1
+            if ((c & 1) == 1) {
+                if ((a & 1) == 0 && (b & 1) == 0) {
+                    flips++;
+                }
+            } else {
+                if ((a & 1) == 1)
+                    flips++;
+                if ((b & 1) == 1)
+                    flips++;
+            }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+
+        return flips;
+
+        // approach 2
+
+        int result = ((a|b)^c);
+
+        int result1 = a&b;
+
+        int result2 = result & result1;
+
+        return __builtin_popcount(result) + __builtin_popcount(result2);
+    }
+};
